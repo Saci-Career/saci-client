77b25116328650ff8a23815704278ce4a4d1c568
plinionr3 <pl1n10nr3@gmail.com>
2024-07-01 13:50:12 +0100

    Apply transitions when user navigate between steps

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,68 @@
 <template>
-  <div class="Container">
-    <img src="@/assets/images/logo-and-lettering.svg" alt="Saci Logo" class="Logo" />
-    <h1>Choose the role you want</h1>
-    <div class="Dropdown">
-      <ElSelect v-model="localSelectedRole" placeholder="Select a role" size="large">
-        <ElOption
-          v-for="item in rolesStore.roles"
-          :key="item.name"
-          :label="item.label"
-          :value="item.name"
+  <div>
+    <div class="Container" ref="roleSectionRef">
+      <img src="@/assets/images/logo-and-lettering.svg" alt="Saci Logo" class="Logo" />
+      <h1>Choose the role you want</h1>
+      <div class="Dropdown">
+        <ElSelect
+          v-model="localSelectedRole"
+          placeholder="Select a role"
+          size="large"
           @change="handleRoleChange"
-        />
-      </ElSelect>
-    </div>
-  </div>
-
-  <div v-if="role && role.knowledges?.length" class="KnowledgesContainer">
-    <h2>
-      Now, level the ability you think you <br />
-      have in each technology below. 0 you <br />know nothing, and 5 you master the knowledge
-    </h2>
-    <div v-for="(knowledge, index) in role.knowledges" :key="index" class="KnowledgeItem">
-      <div class="KnowledgeName">{{ knowledge.name }}</div>
-      <div class="KnowledgeInputs">
-        <ElSlider
-          v-model="quizStore.quizAnswers.answers[index].answer"
-          :min="0"
-          :max="5"
-          :step="1"
-          show-stops
-        />
+        >
+          <ElOption
+            v-for="item in rolesStore.roles"
+            :key="item.name"
+            :label="item.label"
+            :value="item.name"
+          />
+        </ElSelect>
       </div>
     </div>
-    <ElButton type="primary" @click="getResult()">Get Result</ElButton>
-  </div>
 
-  <div v-if="quizStore.quizResult" class="ResultScreen">
-    <img src="@/assets/images/logo-and-lettering.svg" alt="Saci Logo" class="Logo" />
-    <p>
-      Considering your responses, you are considered<br />
-      a <strong>{{ quizStore.quizResult.currentLevel?.name }}</strong
-      >!
-    </p>
-    <p v-if="quizStore.quizResult.nextLevel">
-      If you want to evolve technically, click
-      <a :href="quizStore.quizResult.nextLevel.link">here</a> to get access<br />
-      to a technical knowledge path
-    </p>
-    <p v-else>You are currently at the highest level.</p>
+    <div
+      v-if="role && role.knowledges && role.knowledges.length"
+      class="KnowledgeContainer"
+      ref="knowledgeSectionRef"
+    >
+      <h2>
+        Now, level the ability you think you <br />
+        have in each technology below. 0 you <br />know nothing, and 5 you master the knowledge
+      </h2>
+      <div v-for="(knowledge, index) in role.knowledges" :key="index" class="KnowledgeItem">
+        <div class="KnowledgeName">{{ knowledge.name }}</div>
+        <div class="KnowledgeInputs">
+          <ElSlider
+            v-model="quizStore.quizAnswers.answers[index].answer"
+            :min="0"
+            :max="5"
+            :step="1"
+            show-stops
+          />
+        </div>
+      </div>
+      <ElButton type="primary" @click="getResult()">Get Result</ElButton>
+    </div>
+
+    <div v-if="quizStore.quizResult" class="ResultScreen" ref="resultSectionRef">
+      <img src="@/assets/images/logo-and-lettering.svg" alt="Saci Logo" class="Logo" />
+      <p>
+        Considering your responses, you are considered<br />
+        a <strong>{{ quizStore.quizResult.currentLevel?.name }}</strong
+        >!
+      </p>
+      <p v-if="quizStore.quizResult.nextLevel">
+        If you want to evolve technically, click
+        <a :href="quizStore.quizResult.nextLevel.link">here</a> to get access<br />
+        to a technical knowledge path
+      </p>
+      <p v-else>You are currently at the highest level.</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted, nextTick } from 'vue'
 import { ElOption, ElSelect, ElSlider, ElButton } from 'element-plus'
 import { useRolesStore } from '@/stores/roles'
 import { useQuizStore } from '@/stores/quiz'
@@ -65,22 +75,40 @@ const quizStore = useQuizStore()
 const role = ref<Role | null>(null)
 const localSelectedRole = ref<string | null>(null)
 
+const roleSectionRef = ref<HTMLElement | null>(null)
+const knowledgeSectionRef = ref<HTMLElement | null>(null)
+const resultSectionRef = ref<HTMLElement | null>(null)
+
 onMounted(async () => {
   await rolesStore.fetchRoles()
   if (rolesStore.roles.length > 0) {
     localSelectedRole.value = ''
-    handleRoleChange(rolesStore.roles[0])
   }
 })
 
-const handleRoleChange = (selectedRole: Role) => {
+const handleRoleChange = async (roleName: string) => {
+  const selectedRole = rolesStore.roles.find((role) => role.name === roleName)
+  if (!selectedRole) {
+    ErrorMessage('Selected role does not exist')
+    return
+  }
   try {
     role.value = selectedRole
     quizStore.initializeQuizAnswers(selectedRole.knowledges)
     quizStore.setSelectedRole(selectedRole)
+    await nextTick()
+    scrollToKnowledgeSection()
   } catch (error) {
     ErrorMessage('Failed to set selected role')
   }
+}
+
+const scrollToKnowledgeSection = () => {
+  knowledgeSectionRef.value?.scrollIntoView({ behavior: 'smooth' })
+}
+
+const scrollToResultSection = () => {
+  resultSectionRef.value?.scrollIntoView({ behavior: 'smooth' })
 }
 
 const getResult = async () => {
@@ -91,6 +119,7 @@ const getResult = async () => {
   SuccessMessage('Fetching your results...')
   try {
     await quizStore.updateQuizResult(quizStore.quizAnswers)
+    scrollToResultSection()
   } catch (error) {
     ErrorMessage('Error getting your result')
   }
@@ -98,16 +127,8 @@ const getResult = async () => {
 </script>
 
 <style scoped lang="scss">
-.Container {
-  width: 800px;
-  height: auto;
-  margin: auto;
-  text-align: center;
-  margin-bottom: 100vh;
-  margin-top: 30vh;
-}
-
-.KnowledgesContainer {
+.Container,
+.KnowledgeContainer {
   width: 800px;
   height: auto;
   margin: auto;
@@ -133,6 +154,7 @@ h1 {
 
 h2 {
   text-align: center;
+  padding-top: 60px;
   padding-bottom: 60px;
 }
 
@@ -166,10 +188,5 @@ h2 {
   align-items: center;
   gap: 20px;
   text-align: center;
-}
-
-.ResultScreen .Logo {
-  width: 100px;
-  margin-bottom: 20px;
 }
 </style>
